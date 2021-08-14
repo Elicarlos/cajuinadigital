e9f9ddb84fcdf1e2990b7ff4f689c9a58b633de3
Elicarlos Ferreira <elicarlosantos_@hotmail.com>
2021-08-14 02:59:42 -0300

    Revert "Staging"

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    template_name = "index.html"
-    return render(request, template_name)
+    return HttpResponse('Ola mundo !!!!!!!!!!!!!!!!!!!')
